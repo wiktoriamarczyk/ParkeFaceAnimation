a72dc310ee70b24c65b74892fe241ba6841555a4
Wiktoria Marczyk <wiktoriam.278@gmail.com>
2025-04-25 22:51:08 +0200

    Project files

---
+++ Castaway/Assets/Character and Animation/Facial_Audio.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+public var impact: AudioClip;
+var lipsync_audio: AudioSource;
+function Start() {
+    lipsync_audio = GetComponent.<AudioSource>();
+}
+function PlayAudio() {
+    lipsync_audio.PlayOneShot(impact);
+}
+function StopAudio() {
+    lipsync_audio.Stop();
+}
